ee73ec2f85d8717cccda743b21f7f8ebecec4646
Cotty2 <cotty2322@gmail.com>
2025-02-28 23:13:50 +1000

    Add files via upload

--- laba3/main.c
+++ laba3/main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[80];
+    int i = 0;
+
+    printf("Vvedite stroku: ");
+    gets(str);
+
+    while (str[i] != '\0') {
+        if (str[i] == 'a' || str[i] == 'b') {
+            str[i] = str[i] - 32;
+        }
+        i++;
+    }
+
+    printf("Resultat: %s\n", str);
+    return 0;
+}
 
