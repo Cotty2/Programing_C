b599b79256f3e971fc53ebf9605d821118af35c7
cotty2 <cotty2322@gmail.com>
2025-03-15 13:14:56 +1000

    1

--- dlab1/main.c
+++ dlab1/main.c
@@ -1,55 +1,54 @@
-#define SETBIT(b, k) ((0200 >> (k)) | (b))
-#define CLRBIT(b, k) (~(0200 >> (k)) & (b))
-#define TESTBIT(b, k) (((0200 >> (k)) & (b)) ? 1 : 0)
-unsigned char nat[100000000];
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-#define DEBUG
-//#undef DEBUG
+#define SETBIT(b, k) ((0200 >> (k)) | (b))
+#define CLRBIT(b, k) (~(0200 >> (k)) & (b))
+#define TESTBIT(b, k) (((0200 >> (k)) & (b)) ? 1 : 0)
 
-int main(int argc, char *argv[])
-{  
+int main(int argc, char *argv[]) {  
     long N;
-    printf("vvedite N:\n");
+    printf("Введите N:\n");
     scanf("%ld", &N);
-  
-    int i, j;
 
-    for (i = 0; i < N / 8; i++)
-        nat[i] = 0377; 
-  
-    #ifdef DEBUG
-    nat[0] = 0; 
-    nat[0] = SETBIT(nat[0], 4);
-    nat[0] = SETBIT(nat[0], 5);
-    printf("%x\n", nat[0]);
-  
-    for (i = 0; i < 8; i++)
-        putchar(TESTBIT(nat[0], i) ? '1' : '0');
-    putchar('\n');
-    #endif
+    if (N < 2) {
+        printf("Нет простых чисел, меньших или равных %ld.\n", N);
+        return 0;
+    }
 
+    size_t nat_size = (N + 7) / 8;
+    unsigned char *nat = malloc(nat_size);
+    if (!nat) {
+        printf("Ошибка выделения памяти.\n");
+        return 1;
+    }
+
+    // Инициализация всех битов в 1
+    for (size_t i = 0; i < nat_size; i++) {
+        nat[i] = 0377; // 0377 в восьмеричной системе = 0xFF
+    }
+
+    // Установка битов для 0 и 1 в 0 (они не являются простыми числами)
     nat[0] = CLRBIT(nat[0], 0);
     nat[0] = CLRBIT(nat[0], 1);
 
     long sqrtN = (long)sqrt(N);
-    for (i = 2; i <= sqrtN; i++) {
+    for (long i = 2; i <= sqrtN; i++) {
         if (TESTBIT(nat[i / 8], i % 8)) {
-            for (j = i * i; j <= N; j += i) {
+            for (long j = i * i; j <= N; j += i) {
                 nat[j / 8] = CLRBIT(nat[j / 8], j % 8);
             }
         }
     }
 
-    printf("Prostye chisla do %ld:\n", N);
-    for (i = 2; i <= N; i++) {
+    printf("Простые числа до %ld:\n", N);
+    for (long i = 2; i <= N; i++) {
         if (TESTBIT(nat[i / 8], i % 8)) {
-            printf("%d ", i);
+            printf("%ld ", i);
         }
     }
     printf("\n");
 
+    free(nat);
     return 0;
 }
