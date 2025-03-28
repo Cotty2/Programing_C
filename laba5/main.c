c6bacdfb270de4ff1963a45abe6a1e065c537110
cotty2 <denis232@lenta.ru>
2025-03-28 19:02:15 +1000

    1

--- laba5/main.c
+++ laba5/main.c
@@ -19,12 +19,47 @@ double** summatrix(double **m1, double **m2, int N) {
     return temp;
 }
 
+double** submatrix(double **m1, double **m2, int N) {  
+    double **temp;
+    int i, j;
+
+    temp = (double**)malloc(N * sizeof(double*));
+    for (i = 0; i < N; i++) {
+        temp[i] = (double*)malloc(N * sizeof(double));
+    }
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            temp[i][j] = m1[i][j] - m2[i][j];  
+        }
+    }
+
+    return temp;
+}
+double** multmatrix(double **m1, double **m2, int N) {
+    double **temp;
+    int i, j, k;
+
+    temp = (double**)malloc(N * sizeof(double*));
+    for (i = 0; i < N; i++) {
+        temp[i] = (double*)malloc(N * sizeof(double));
+    }
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            for (k = 0; k < N; k++) {
+                temp[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+
+    return temp;
+}
 int main() {
     double **m1, **m2, **result;
     int N, i, j;
     char operation;
 
-    printf("Введите размерность матриц (n): ");
+    printf("vvedite matrisu (n): ");
     scanf("%d", &N);
 
     m1 = (double**)malloc(N * sizeof(double*));
@@ -37,7 +72,7 @@ int main() {
         m2[i] = (double*)malloc(N * sizeof(double));
     }
 
-    printf("Введите элементы первой матрицы:\n");
+    printf("vvedite 1 matrisu:\n");
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             printf("m1[%d][%d] = ", i, j);
@@ -45,7 +80,7 @@ int main() {
         }
     }
 
-    printf("Введите элементы второй матрицы:\n");
+    printf("vvedite 2 matrisu:\n");
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             printf("m2[%d][%d] = ", i, j);
@@ -53,17 +88,23 @@ int main() {
         }
     }
 
-    printf("Введите операцию (+, -, *): ");
+    printf("vvedite operasiu (+, -, *): ");
     scanf(" %c", &operation);
 
     if (operation == '+') {
         result = summatrix(m1, m2, N);
+    } 
+     else if (operation == '-') {  
+        result = submatrix(m1, m2, N); 
+    } 
+     else if (operation == '*') {  
+        result = multmatrix(m1, m2, N);
     } else {
-        printf("Неверная операция!\n");
+        printf("incorrect\n");
         return 1;
     }
 
-    printf("Результат:\n");
+    printf("result:\n");
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             printf("%lf ", result[i][j]);
