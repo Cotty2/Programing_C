5ec4a9fa72a96ee2bdba5f35455acf6545e043f8
cotty2 <cotty2322@gmail.com>
2025-04-28 11:52:08 +1000

    1

--- laba9/main.c
+++ laba9/main.c
@@ -1,33 +1,77 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <ctype.h>
 
 int main() {
-    char inputStr[256];
-    printf("Введите выражение: ");
-    fgets(inputStr, sizeof(inputStr), stdin);
+    char str[256];
+    printf("Введите пример: ");
+    fgets(str, 256, stdin);
+    
+    double result = 0;
+    double num = 0;
+    char op = '+';
+    int i = 0;
+    int error = 0;
 
-    inputStr[strcspn(inputStr, "\n")] = 0;
-
-    char *strok;
-    double result = 0.0;
-    char operation = '+';
-
-    strok = strtok(inputStr, " ");
-    while (strok != NULL) {
-        if (strcmp(strok, "+") == 0 || strcmp(strok, "-") == 0) {
-            operation = strok[0];
-        } else {
-            double number = atof(strok);
-            if (operation == '+') {
-                result += number;
-            } else {
-                result -= number;
+    while (str[i] != '\n' && str[i] != '\0' && !error) {
+        while (str[i] == ' ') i++;
+        
+        if (isdigit(str[i]) || str[i] == '.') {
+            double temp = 0;
+            int dot = 0;
+            double div = 1;
+            
+            while (isdigit(str[i]) || str[i] == '.') {
+                if (str[i] == '.') {
+                    if (dot) { 
+                        printf("два знака точки\n");
+                        error = 1;
+                        break;
+                    }
+                    dot = 1;
+                } else {
+                    if (dot) {
+                        div *= 10;
+                        temp = temp * 10 + (str[i] - '0');
+                    } else {
+                        temp = temp * 10 + (str[i] - '0');
+                    }
+                }
+                i++;
             }
+            
+            if (dot) temp /= div;
+            num = temp;
         }
-        strok = strtok(NULL, " ");
-    }
+        
 
-    printf("Результат: %f\n", result);
+        if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
+            if (op == '+') result += num;
+            else if (op == '-') result -= num;
+            else if (op == '*') result *= num;
+            else if (op == '/') {
+                if (num == 0) {
+                    printf("деление на ноль!\n");
+                    error = 1;
+                } else {
+                    result /= num;
+                }
+            }
+            op = str[i];
+            i++;
+            num = 0;
+        }
+    }
+    
+    if (!error) {
+        if (op == '+') result += num;
+        else if (op == '-') result -= num;
+        else if (op == '*') result *= num;
+        else if (op == '/') {
+            if (num == 0) printf("деление на ноль!\n");
+            else result /= num;
+        }
+    }
+    
+    if (!error) printf("Ответ: %g\n", result);
     return 0;
 }
